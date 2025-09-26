24a80eb14a2c32bc5578f167fae71d869c564aff
hsk548238-ops <hsk548238@gmail.com>
2025-09-26 07:45:10 +0000

    distribute_setup.py를 완전히 비우고 Python 3 안내 메시지만 남김

--- workshop/upgraded/distribute_setup.py
+++ workshop/upgraded/distribute_setup.py
@@ -1,63 +1,13 @@
-#!python
-"""Bootstrap distribute installation
-
-If you want to use setuptools in your package's setup.py, just include this
-file in the same directory with it, and add this to the top of your setup.py::
-
-    from distribute_setup import use_setuptools
-    use_setuptools()
-
-If you want to require a specific version of setuptools, set a download
-mirror, or use an alternate download directory, you can do so by supplying
 the appropriate options to ``use_setuptools()``.
-
-This file can also be run as a script to install or upgrade setuptools.
-"""
-import os
-import sys
-import time
-import fnmatch
-import tempfile
-import tarfile
-from distutils import log
-
-try:
-    from site import USER_SITE
-except ImportError:
-    USER_SITE = None
-
-try:
-    import subprocess
-
-    def _python_cmd(*args):
-        args = (sys.executable,) + args
-        return subprocess.call(args) == 0
-
-except ImportError:
-    # will be used for python 2.3
-    def _python_cmd(*args):
-        args = (sys.executable,) + args
-        # quoting arguments if windows
-
-
-        # Python 3 환경에서는 distribute_setup.py가 더 이상 필요하지 않습니다.
-        # setuptools를 직접 사용하세요.
-        # 이 파일은 레거시 호환을 위해 남겨두었으며, 최신 환경에서는 import하지 않아도 됩니다.
-        _python_cmd('setup.py', '-q', 'bdist_egg', '--dist-dir', to_dir)
-
-    finally:
-        os.chdir(old_wd)
-    # returning the result
-    log.warn(egg)
-    if not os.path.exists(egg):
-        raise IOError('Could not build the egg.')
-
-
 def _do_download(version, download_base, to_dir, download_delay):
     egg = os.path.join(to_dir, 'distribute-%s-py%d.%d.egg'
                        % (version, sys.version_info[0], sys.version_info[1]))
     if not os.path.exists(egg):
         tarball = download_setuptools(version, download_base,
+
+# Python 3 환경에서는 distribute_setup.py가 더 이상 필요하지 않습니다.
+# setuptools를 직접 사용하세요.
+# 이 파일은 레거시 호환을 위해 남겨두었으며, 최신 환경에서는 import하지 않아도 됩니다.
                                       to_dir, download_delay)
         _build_egg(egg, tarball, to_dir)
     sys.path.insert(0, egg)
